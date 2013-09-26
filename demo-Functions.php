29220857c5237b9a5afecf913678dc20583bc9f4
Kate Cooper <kate.cooper@mac.com>
2013-09-26 07:17:34 +0100

    Adding Functions PHP

---
+++ demo-Functions.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+
+</head>
+	<?php
+	require_once('logic-Functions.php');
+	?>
+<body>
+
+	Your total is 
+	<?=calculate_total(10,3,'priority')?>
+
+</body>
+</html> 
