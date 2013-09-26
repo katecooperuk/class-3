48369106d229de7480b8d84df191e9245a8a71c4
Kate Cooper <kate.cooper@mac.com>
2013-09-26 07:16:39 +0100

    Adding Basics PHP

---
+++ demo-Basics.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+
+</head>
+	<?php
+	require_once('logic-Basics.php');
+	?>
+<body>
+
+	The Square of 4 is <?=$square?>
+	The Cube of 4 is <?=$cube?>
+	My favorite color is <?php echo $favorite_color; ?>
+	My favorite color is <?=$favorite_color;?>
+
+</body>
+</html> 
