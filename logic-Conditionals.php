975dddba6e431a1106a8d7bdf10d82f8698f9845
Kate Cooper <kate.cooper@mac.com>
2013-09-26 07:17:04 +0100

    Adding Conditionals PHP

---
+++ logic-Conditionals.php
@@ -0,0 +1,17 @@
+<?php
+$age = 15;
+
+if($age <= 12) {
+	$person_type = 'kiddo';
+}
+else if($age > 12 && $age <= 19) {
+	$person_type = 'teenager';
+}
+else if($age > 19 && $age <= 80) {
+	$person_type = 'adult';
+}
+else {
+	$person_type = 'super wise person';
+}
+
+?>
