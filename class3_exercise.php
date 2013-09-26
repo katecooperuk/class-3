25dcdb4c7ef6a201dc98124f6b77583f9e5112ba
Kate Cooper <kate.cooper@mac.com>
2013-09-26 07:16:15 +0100

    Adding Day/Night exercise PHP

---
+++ class3_exercise.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+
+<?php
+	require_once('class3_logic.php');
+	?>
+
+<style type='text/css'>
+       body {
+           background-color:<?=$background_color?>;
+           color: #e541b6;
+           font-size: 16px;
+        }
+</style>
+
+	
+
+</head>
+
+<body>
+
+	The current time is: <?=$timestamp?>
+	<br>
+	The timezone is London, UK.
+
+</body>
+</html> 
