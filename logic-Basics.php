306d1eedfda023d4e94d129584dc8a3c252bb3c9
Kate Cooper <kate.cooper@mac.com>
2013-09-26 07:16:46 +0100

    Adding Basics PHP

---
+++ logic-Basics.php
@@ -0,0 +1,17 @@
+<?php
+# Single line comment
+// Single line comment
+$square = 4 * 4;
+$cube = 4 * 4 * 4;
+
+$favorite_color = "red";
+
+$username = 'bob34';
+
+# examples below showing use of inverted commas
+$sent = "He walked into the store and said \"hello\"";
+$sent = 'He walked into the store and said "hello"';
+
+$age =15;
+
+?>
