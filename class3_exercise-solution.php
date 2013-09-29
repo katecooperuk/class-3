a7417173aeff3134b99e865517fc5702fa7cb9f3
Kate Cooper <kate.cooper@mac.com>
2013-09-29 07:53:28 +0100

    updating php

---
+++ class3_exercise-solution.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<?php
+	require('class3_logic-solution.php');
+	?>
+	
+	<link rel='stylesheet' href="day-night.css" type="text">
+	
+</head>
+
+<body>
+
+
+
+
+</body>
+</html>
